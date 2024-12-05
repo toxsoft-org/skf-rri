f47da3b1f44986131ff8e9f0a2909694e4bc6abc
Yoshimuto <tdo@toxsoft.ru>
2024-12-05 17:36:02 +0400

    L10n

--- org.toxsoft.skf.rri.struct.gui/src/org/toxsoft/skf/rri/struct/gui/km5/ISkResources.java
+++ org.toxsoft.skf.rri.struct.gui/src/org/toxsoft/skf/rri/struct/gui/km5/ISkResources.java
@@ -12,40 +12,40 @@ interface ISkResources {
   // ----------------------------------------
   // AttributeModel
 
-  String STR_N_ATTR_ID          = Messages.getString( "STR_N_ATTR_ID" );         // "Идентификатор";
-  String STR_D_ATTR_ID          = Messages.getString( "STR_D_ATTR_ID" );         // "Идентификатор атрибута";
-  String STR_D_ATTR_TYPE        = Messages.getString( "STR_D_ATTR_TYPE" );       // "Тип атрибута";
-  String STR_N_ATTR_TYPE        = Messages.getString( "STR_N_ATTR_TYPE" );       // "Тип";
-  String STR_N_ATTR_NAME        = Messages.getString( "STR_N_ATTR_NAME" );       // "Наименование";
-  String STR_D_ATTR_NAME        = Messages.getString( "STR_D_ATTR_NAME" );       // "Наименование атрибута";
-  String STR_D_ATTR_DESCRIPTION = Messages.getString( "STR_D_ATTR_DESCRIPTION" );// "Описание атрибута";
-  String STR_N_ATTR_DESCRIPTION = Messages.getString( "STR_N_ATTR_DESCRIPTION" );// "Описание";
-  String STR_N_ATTRIBUTES_LIST  = Messages.getString( "STR_N_ATTRIBUTES_LIST" ); // "Список атрибутов";
-  String STR_D_ATTRIBUTES_LIST  = Messages.getString( "STR_D_ATTRIBUTES_LIST" ); // "Список НСИ атрибутов класса";
+  String STR_N_ATTR_ID          = Messages.getString( "STR_N_ATTR_ID" );
+  String STR_D_ATTR_ID          = Messages.getString( "STR_D_ATTR_ID" );
+  String STR_D_ATTR_TYPE        = Messages.getString( "STR_D_ATTR_TYPE" );
+  String STR_N_ATTR_TYPE        = Messages.getString( "STR_N_ATTR_TYPE" );
+  String STR_N_ATTR_NAME        = Messages.getString( "STR_N_ATTR_NAME" );
+  String STR_D_ATTR_NAME        = Messages.getString( "STR_D_ATTR_NAME" );
+  String STR_D_ATTR_DESCRIPTION = Messages.getString( "STR_D_ATTR_DESCRIPTION" );
+  String STR_N_ATTR_DESCRIPTION = Messages.getString( "STR_N_ATTR_DESCRIPTION" );
+  String STR_N_ATTRIBUTES_LIST  = Messages.getString( "STR_N_ATTRIBUTES_LIST" );
+  String STR_D_ATTRIBUTES_LIST  = Messages.getString( "STR_D_ATTRIBUTES_LIST" );
 
   //
   // ----------------------------------------
   // LinkModel
 
-  String STR_N_LINKS_LIST          = Messages.getString( "STR_N_LINKS_LIST" );         // "Список связей";
-  String STR_D_LINKS_LIST          = Messages.getString( "STR_D_LINKS_LIST" );         // "Список НСИ связей класса";
-  String STR_N_LINK_CLASS_IDS      = Messages.getString( "STR_N_LINK_CLASS_IDS" );     // "Классы связи";
-  String STR_D_LINK_CLASS_IDS      = Messages.getString( "STR_D_LINK_CLASS_IDS" );     // "Возможные классы связи";
-  String STR_N_LINK_IS_EXACT_COUNT = Messages.getString( "STR_N_LINK_IS_EXACT_COUNT" );// "Кол-во связей точное";
-  String STR_D_LINK_IS_EXACT_COUNT = Messages.getString( "STR_D_LINK_IS_EXACT_COUNT" );// "Кол-во связей точное";
-  String STR_N_LINK_MAX_COUNT      = Messages.getString( "STR_N_LINK_MAX_COUNT" );     // "Макс. кол-во связей";
-  String STR_D_LINK_MAX_COUNT      = Messages.getString( "STR_D_LINK_MAX_COUNT" );     // "Максимальное кол-во связей";
-  String STR_D_LINK_DESCRIPTION    = Messages.getString( "STR_D_LINK_DESCRIPTION" );   // "Описание связи";
-  String STR_N_LINK_DESCRIPTION    = Messages.getString( "STR_N_LINK_DESCRIPTION" );   // "Описание";
-  String STR_N_LINK_NAME           = Messages.getString( "STR_N_LINK_NAME" );          // "Наименование";
-  String STR_D_LINK_NAME           = Messages.getString( "STR_D_LINK_NAME" );          // "Наименование связи";
-  String STR_D_LINK_ID             = Messages.getString( "STR_D_LINK_ID" );            // "Идентификатор связи";
-  String STR_N_LINK_ID             = Messages.getString( "STR_N_LINK_ID" );            // "Идентификатор";
+  String STR_N_LINKS_LIST          = Messages.getString( "STR_N_LINKS_LIST" );
+  String STR_D_LINKS_LIST          = Messages.getString( "STR_D_LINKS_LIST" );
+  String STR_N_LINK_CLASS_IDS      = Messages.getString( "STR_N_LINK_CLASS_IDS" );
+  String STR_D_LINK_CLASS_IDS      = Messages.getString( "STR_D_LINK_CLASS_IDS" );
+  String STR_N_LINK_IS_EXACT_COUNT = Messages.getString( "STR_N_LINK_IS_EXACT_COUNT" );
+  String STR_D_LINK_IS_EXACT_COUNT = Messages.getString( "STR_D_LINK_IS_EXACT_COUNT" );
+  String STR_N_LINK_MAX_COUNT      = Messages.getString( "STR_N_LINK_MAX_COUNT" );
+  String STR_D_LINK_MAX_COUNT      = Messages.getString( "STR_D_LINK_MAX_COUNT" );
+  String STR_D_LINK_DESCRIPTION    = Messages.getString( "STR_D_LINK_DESCRIPTION" );
+  String STR_N_LINK_DESCRIPTION    = Messages.getString( "STR_N_LINK_DESCRIPTION" );
+  String STR_N_LINK_NAME           = Messages.getString( "STR_N_LINK_NAME" );
+  String STR_D_LINK_NAME           = Messages.getString( "STR_D_LINK_NAME" );
+  String STR_D_LINK_ID             = Messages.getString( "STR_D_LINK_ID" );
+  String STR_N_LINK_ID             = Messages.getString( "STR_N_LINK_ID" );
 
   //
   // --------------------------------------
   // RriSectionModel
 
-  String STR_N_RRI_SECTIONS_LIST = Messages.getString( "STR_N_RRI_SECTIONS_LIST" );// "RRI sections: ";
-  String STR_D_RRI_SECTIONS_LIST = Messages.getString( "STR_D_RRI_SECTIONS_LIST" );// "Reg Ref Info sections: ";
+  String STR_N_RRI_SECTIONS_LIST = Messages.getString( "STR_N_RRI_SECTIONS_LIST" );
+  String STR_D_RRI_SECTIONS_LIST = Messages.getString( "STR_D_RRI_SECTIONS_LIST" );
 }
--- org.toxsoft.skf.rri.struct.gui/src/org/toxsoft/skf/rri/struct/gui/panels/ISkResources.java
+++ org.toxsoft.skf.rri.struct.gui/src/org/toxsoft/skf/rri/struct/gui/panels/ISkResources.java
@@ -15,20 +15,20 @@ interface ISkResources {
   // ----------------------------------------
   // SelectRriSectionToolbarComposite
 
-  String STR_N_SELECT_RRI_SECTION  = Messages.getString( "STR_N_SELECT_RRI_SECTION" );// "Выбрать раздел НСИ";
-  String STR_D_SELECT_RRI_SECTION  = Messages.getString( "STR_D_SELECT_RRI_SECTION" );//"Выбрать раздел НСИ для редактирования";
-  String STR_NOT_SELECTED          = Messages.getString( "STR_NOT_SELECTED" );//"не выбран";
-  String STR_SELECTION_RRI_SECTION = Messages.getString( "STR_SELECTION_RRI_SECTION" );// "Выбор раздела НСИ";
-  String STR_RRI_SECTION           = Messages.getString( "STR_RRI_SECTION" );//"Раздел НСИ: ";
+  String STR_N_SELECT_RRI_SECTION  = Messages.getString( "STR_N_SELECT_RRI_SECTION" );
+  String STR_D_SELECT_RRI_SECTION  = Messages.getString( "STR_D_SELECT_RRI_SECTION" );
+  String STR_NOT_SELECTED          = Messages.getString( "STR_NOT_SELECTED" );
+  String STR_SELECTION_RRI_SECTION = Messages.getString( "STR_SELECTION_RRI_SECTION" );
+  String STR_RRI_SECTION           = Messages.getString( "STR_RRI_SECTION" );
 
   //
   // --------------------------------------
   // PanelRriSectionStructEditor
 
-  String IT_NEEDS_TO_CREATE_AT_LEAST_ONE_RRI_ATTR = Messages.getString( "IT_NEEDS_TO_CREATE_AT_LEAST_ONE_RRI_ATTR" );//"Необходимо создать хотя бы один атрибут НСИ для нового класса";
-  String STR_RRI_ATTR_CREATION                    = Messages.getString( "STR_RRI_ATTR_CREATION" );//"Создание атрибута НСИ";
-  String STR_SELECTION_CLASS_TO_ADD_INTO_RRI      = Messages.getString( "STR_SELECTION_CLASS_TO_ADD_INTO_RRI" );// "Выбор класса для НСИ";
-  String STR_RRI_CLASSES                          = Messages.getString( "STR_RRI_CLASSES" );// "Классы НСИ: ";
-  String STR_RRI_ATTRS                            = Messages.getString( "STR_RRI_ATTRS" );// "Атрибуты НСИ: ";
-  String STR_RRI_LINKS                            = Messages.getString( "STR_RRI_LINKS" );// "Связи НСИ: ";
+  String IT_NEEDS_TO_CREATE_AT_LEAST_ONE_RRI_ATTR = Messages.getString( "IT_NEEDS_TO_CREATE_AT_LEAST_ONE_RRI_ATTR" );
+  String STR_RRI_ATTR_CREATION                    = Messages.getString( "STR_RRI_ATTR_CREATION" );
+  String STR_SELECTION_CLASS_TO_ADD_INTO_RRI      = Messages.getString( "STR_SELECTION_CLASS_TO_ADD_INTO_RRI" );
+  String STR_RRI_CLASSES                          = Messages.getString( "STR_RRI_CLASSES" );
+  String STR_RRI_ATTRS                            = Messages.getString( "STR_RRI_ATTRS" );
+  String STR_RRI_LINKS                            = Messages.getString( "STR_RRI_LINKS" );
 }
