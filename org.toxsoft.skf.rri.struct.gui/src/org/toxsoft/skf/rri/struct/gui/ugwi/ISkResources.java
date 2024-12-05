8c6a41075d07c1a733f78da6d32f928bd7714628
Yoshimuto <tdo@toxsoft.ru>
2024-12-05 16:48:30 +0400

    L10n

--- org.toxsoft.skf.rri.struct.gui/src/org/toxsoft/skf/rri/struct/gui/ugwi/ISkResources.java
+++ org.toxsoft.skf.rri.struct.gui/src/org/toxsoft/skf/rri/struct/gui/ugwi/ISkResources.java
@@ -10,13 +10,11 @@ interface ISkResources {
   /**
    * {@link SingleRriPropUgwiSelectPanel}
    */
-  String MSG_NO_SEL_CLASS = "No selected RRI class";
-  String MSG_NO_SEL_OBJ   = "No selected RRI object";
-  String MSG_NO_SEL_PROP  = "No selected RRI property";
-
-  String STR_N_CLASS_PROP_KIND = "Type of RRI property";
-  String STR_D_CLASS_PROP_KIND = "Type of RRI  class property";
-
-  String STR_RRI_SECTION = "RRI section: ";
+  String MSG_NO_SEL_CLASS      = Messages.getString( "MSG_NO_SEL_CLASS" );      //$NON-NLS-1$
+  String MSG_NO_SEL_OBJ        = Messages.getString( "MSG_NO_SEL_OBJ" );        //$NON-NLS-1$
+  String MSG_NO_SEL_PROP       = Messages.getString( "MSG_NO_SEL_PROP" );       //$NON-NLS-1$
+  String STR_CLASS_PROP_KIND   = Messages.getString( "STR_CLASS_PROP_KIND" );   //$NON-NLS-1$
+  String STR_CLASS_PROP_KIND_D = Messages.getString( "STR_CLASS_PROP_KIND_D" ); //$NON-NLS-1$
+  String STR_RRI_SECTION       = Messages.getString( "STR_RRI_SECTION" );       //$NON-NLS-1$
 
 }
