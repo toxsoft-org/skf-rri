751f37c4d105109acbdfd03c3337bd345c47de2b
Yoshimuto <tdo@toxsoft.ru>
2024-12-05 17:49:49 +0400

    L10n

--- org.toxsoft.skf.rri.values.gui/src/org/toxsoft/skf/rri/values/gui/panels/ITsResources.java
+++ org.toxsoft.skf.rri.values.gui/src/org/toxsoft/skf/rri/values/gui/panels/ITsResources.java
@@ -12,12 +12,12 @@ public interface ITsResources {
   // ---------------------------------------------------------
   // PanelRriSectionValuesEditor
 
-  String LINKS_TAB_NAME           = Messages.getString( "LINKS_TAB_NAME" );
-  String ATTRS_TAB_NAME           = Messages.getString( "ATTRS_TAB_NAME" );
-  String TREE_MODE_NAME           = Messages.getString( "TREE_MODE_NAME" );
-  String STR_N_SELECT_RRI_SECTION = Messages.getString( "STR_N_SELECT_RRI_SECTION" );
-  String STR_D_SELECT_RRI_SECTION = Messages.getString( "STR_D_SELECT_RRI_SECTION" );
-  String STR_RRI_CLASSES          = Messages.getString( "STR_RRI_CLASSES" );
+  String LINKS_TAB_NAME           = Messages.getString( "LINKS_TAB_NAME" );           //$NON-NLS-1$
+  String ATTRS_TAB_NAME           = Messages.getString( "ATTRS_TAB_NAME" );           //$NON-NLS-1$
+  String TREE_MODE_NAME           = Messages.getString( "TREE_MODE_NAME" );           //$NON-NLS-1$
+  String STR_N_SELECT_RRI_SECTION = Messages.getString( "STR_N_SELECT_RRI_SECTION" ); //$NON-NLS-1$
+  String STR_D_SELECT_RRI_SECTION = Messages.getString( "STR_D_SELECT_RRI_SECTION" ); //$NON-NLS-1$
+  String STR_RRI_CLASSES          = Messages.getString( "STR_RRI_CLASSES" );          //$NON-NLS-1$
 
   //
   // ---------------------------------------------------------
