ce06c37feb16bfadd0d40a77b024f2cb16f9db36
Yoshimuto <tdo@toxsoft.ru>
2024-12-05 16:41:57 +0400

    L10n

--- org.toxsoft.skf.rri.lib/src/org/toxsoft/skf/rri/lib/impl/ISkResources.java
+++ org.toxsoft.skf.rri.lib/src/org/toxsoft/skf/rri/lib/impl/ISkResources.java
@@ -8,37 +8,36 @@ package org.toxsoft.skf.rri.lib.impl;
 @SuppressWarnings( "nls" )
 interface ISkResources {
 
-  String FMT_ERR_CLASS_IS_REGREF_OWNED     = "Класс %s управлентся службой НСИ";
-  String FMT_ERR_SECTION_ID_ALREADY_ESISTS = "Раздел с идентификатором %s уже существует";
-  String FMT_ERR_INV_SECTION_ID            = "Раздел с идентификатором %s уже существует";
-  String FMT_ERR_EMPTY_NAME                = "Нужно задать название раздела";
-  String FMT_ERR_SECTION_ID_NOT_ESISTS     = "Раздел с идентификатором %s не существует";
-  String STR_LINK_RRI_COMPANION_MASTER     = "Мастер";
-  String STR_LINK_RRI_COMPANION_MASTER_D   = "Мастер-объект для которого сформировано НСИ";
-  String STR_ATTR_RRI_SECTION_PARAMS       = "Параметры";
-  String STR_ATTR_RRI_SECTION_PARAMS_D     = "Набор параметров раздела НСИ и их значений";
-  String STR_TYPE_OPRION_SET               = "Опции";
-  String STR_TYPE_OPRION_SET_D             = "Тип данных, хранящийнабор опции (идентифицированных атомарных значений";
-
-  String STR_EVENT_RRI_EDIT       = "Правка НСИ";
-  String STR_EVENT_RRI_EDIT_D     = "Событие редактирования значения параметра НСИ";
-  String STR_EVPRM_REASON         = "Причина";
-  String STR_EVPRM_REASON_D       = "Причина изменения параметров НСИ";
-  String STR_EVPRM_AUTHOR_LOGIN   = "Автор";
-  String STR_EVPRM_AUTHOR_LOGIN_D = "Автор (точнее, логин, имя входа) изменения параметров НСИ";
-  String STR_EVPRM_SECTION_ID     = "Раздел";
-  String STR_EVPRM_SECTION_ID_D   = "Идентификатор раздела НСИ";
-  String STR_EVPRM_PARAM_ID       = "Параметр";
-  String STR_EVPRM_PARAM_ID_D     = "Идентификатор параметра НСИ";
-  String STR_EVPRM_IS_LINK        = "Связь?";
-  String STR_EVPRM_IS_LINK_D      = "Признак, что параметр НСИ связь, а не атрибут объекта";
-  String STR_EVPRM_OLD_VAL_ATTR   = "Предыдущее";
-  String STR_EVPRM_OLD_VAL_ATTR_D = "Предыдущее значение параметра НСИ";
-  String STR_EVPRM_OLD_VAL_LINK   = "Предыдущее";
-  String STR_EVPRM_OLD_VAL_LINK_D = "Предыдущее значение параметра НСИ";
-  String STR_EVPRM_NEW_VAL_ATTR   = "Новое";
-  String STR_EVPRM_NEW_VAL_ATTR_D = "Новое значение параметра НСИ";
-  String STR_EVPRM_NEW_VAL_LINK   = "Новое";
-  String STR_EVPRM_NEW_VAL_LINK_D = "Новое значение параметра НСИ";
+  String FMT_ERR_CLASS_IS_REGREF_OWNED     = Messages.getString( "FMT_ERR_CLASS_IS_REGREF_OWNED" );     //$NON-NLS-1$
+  String FMT_ERR_SECTION_ID_ALREADY_ESISTS = Messages.getString( "FMT_ERR_SECTION_ID_ALREADY_ESISTS" ); //$NON-NLS-1$
+  String FMT_ERR_INV_SECTION_ID            = Messages.getString( "FMT_ERR_INV_SECTION_ID" );            //$NON-NLS-1$
+  String FMT_ERR_EMPTY_NAME                = Messages.getString( "FMT_ERR_EMPTY_NAME" );                //$NON-NLS-1$
+  String FMT_ERR_SECTION_ID_NOT_ESISTS     = Messages.getString( "FMT_ERR_SECTION_ID_NOT_ESISTS" );     //$NON-NLS-1$
+  String STR_LINK_RRI_COMPANION_MASTER     = Messages.getString( "STR_LINK_RRI_COMPANION_MASTER" );     //$NON-NLS-1$
+  String STR_LINK_RRI_COMPANION_MASTER_D   = Messages.getString( "STR_LINK_RRI_COMPANION_MASTER_D" );   //$NON-NLS-1$
+  String STR_ATTR_RRI_SECTION_PARAMS       = Messages.getString( "STR_ATTR_RRI_SECTION_PARAMS" );       //$NON-NLS-1$
+  String STR_ATTR_RRI_SECTION_PARAMS_D     = Messages.getString( "STR_ATTR_RRI_SECTION_PARAMS_D" );     //$NON-NLS-1$
+  String STR_TYPE_OPRION_SET               = Messages.getString( "STR_TYPE_OPRION_SET" );               //$NON-NLS-1$
+  String STR_TYPE_OPRION_SET_D             = Messages.getString( "STR_TYPE_OPRION_SET_D" );             //$NON-NLS-1$
+  String STR_EVENT_RRI_EDIT                = Messages.getString( "STR_EVENT_RRI_EDIT" );                //$NON-NLS-1$
+  String STR_EVENT_RRI_EDIT_D              = Messages.getString( "STR_EVENT_RRI_EDIT_D" );              //$NON-NLS-1$
+  String STR_EVPRM_REASON                  = Messages.getString( "STR_EVPRM_REASON" );                  //$NON-NLS-1$
+  String STR_EVPRM_REASON_D                = Messages.getString( "STR_EVPRM_REASON_D" );                //$NON-NLS-1$
+  String STR_EVPRM_AUTHOR_LOGIN            = Messages.getString( "STR_EVPRM_AUTHOR_LOGIN" );            //$NON-NLS-1$
+  String STR_EVPRM_AUTHOR_LOGIN_D          = Messages.getString( "STR_EVPRM_AUTHOR_LOGIN_D" );          //$NON-NLS-1$
+  String STR_EVPRM_SECTION_ID              = Messages.getString( "STR_EVPRM_SECTION_ID" );              //$NON-NLS-1$
+  String STR_EVPRM_SECTION_ID_D            = Messages.getString( "STR_EVPRM_SECTION_ID_D" );            //$NON-NLS-1$
+  String STR_EVPRM_PARAM_ID                = Messages.getString( "STR_EVPRM_PARAM_ID" );                //$NON-NLS-1$
+  String STR_EVPRM_PARAM_ID_D              = Messages.getString( "STR_EVPRM_PARAM_ID_D" );              //$NON-NLS-1$
+  String STR_EVPRM_IS_LINK                 = Messages.getString( "STR_EVPRM_IS_LINK" );                 //$NON-NLS-1$
+  String STR_EVPRM_IS_LINK_D               = Messages.getString( "STR_EVPRM_IS_LINK_D" );               //$NON-NLS-1$
+  String STR_EVPRM_OLD_VAL_ATTR            = Messages.getString( "STR_EVPRM_OLD_VAL_ATTR" );            //$NON-NLS-1$
+  String STR_EVPRM_OLD_VAL_ATTR_D          = Messages.getString( "STR_EVPRM_OLD_VAL_ATTR_D" );          //$NON-NLS-1$
+  String STR_EVPRM_OLD_VAL_LINK            = Messages.getString( "STR_EVPRM_OLD_VAL_LINK" );            //$NON-NLS-1$
+  String STR_EVPRM_OLD_VAL_LINK_D          = Messages.getString( "STR_EVPRM_OLD_VAL_LINK_D" );          //$NON-NLS-1$
+  String STR_EVPRM_NEW_VAL_ATTR            = Messages.getString( "STR_EVPRM_NEW_VAL_ATTR" );            //$NON-NLS-1$
+  String STR_EVPRM_NEW_VAL_ATTR_D          = Messages.getString( "STR_EVPRM_NEW_VAL_ATTR_D" );          //$NON-NLS-1$
+  String STR_EVPRM_NEW_VAL_LINK            = Messages.getString( "STR_EVPRM_NEW_VAL_LINK" );            //$NON-NLS-1$
+  String STR_EVPRM_NEW_VAL_LINK_D          = Messages.getString( "STR_EVPRM_NEW_VAL_LINK_D" );          //$NON-NLS-1$
 
 }
